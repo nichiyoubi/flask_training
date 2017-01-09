c94497704b8a59e80dbacf859c6969408f7c9d8f
宇佐美雅紀 <usamimasanori@usamimasakinoMacBook-Pro.local>
2017-01-09 09:43:28 +0900

    Hello World!

---
+++ hello.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
+    
