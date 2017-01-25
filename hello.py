51afd4c6453640cc26dc1d8a794d73fa9c7f598d
宇佐美雅紀 <usamimasanori@usamimasakinoMacBook-Pro.local>
2017-01-25 23:50:53 +0900

    DB接続に環境変数を利用する

--- hello.py
+++ hello.py
@@ -13,8 +13,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:Masanori1972@localhost/'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:Masanori1972@localhost/'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
