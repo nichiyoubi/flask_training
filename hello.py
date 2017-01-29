45d30c9cece8ed4bfa6df7a5769105f4fd921d52
宇佐美雅紀 <usamimasanori@usamimasakinoMacBook-Pro.local>
2017-01-29 23:52:35 +0900

    全てのapiにセッション確認処理追加

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 # _*_ coding: utf-8 _*_
 
 import os
-from flask import Flask, make_response, render_template, request, redirect, url_for, jsonify
-from flask import session
+from flask import Flask, make_response, render_template, request, redirect
+from flask import url_for, jsonify, session
 import numpy as np
 import pandas as pd
 import matplotlib
@@ -63,8 +63,6 @@ def error_handler(error):
 # ログイン処理
 @app.route('/login', methods=['POST'])
 def login():
-    print request.form['email']
-    print request.form['password']
     if _is_account_valid():
         session['email'] = request.form['email']
         return redirect(url_for('device'))
@@ -98,75 +96,96 @@ def device():
 # グラフ表示
 @app.route('/graph1')
 def graph1():
-    fig = plt.figure()
-    lights = LightValue.query.filter().all()
-    nx = np.array([])
-    ny = np.array([])
-    for x in lights:
-	nx = np.append(nx, x.time)
-	ny = np.append(ny, x.light)
-    plt.plot(nx, ny, label="matplotlib test")
-    strio = StringIO.StringIO()
-    fig.savefig(strio, format="svg")
-    plt.close(fig)
+    # セッションにemailが保存されていなければ表紙ページにリダイレクトする
+    if session.get('email') is not None:
+        fig = plt.figure()
+        lights = LightValue.query.filter().all()
+        nx = np.array([])
+        ny = np.array([])
+        for x in lights:
+	    nx = np.append(nx, x.time)
+	    ny = np.append(ny, x.light)
+        plt.plot(nx, ny, label="matplotlib test")
+        strio = StringIO.StringIO()
+        fig.savefig(strio, format="svg")
+        plt.close(fig)
 
-    strio.seek(0)
-    svgstr = strio.buf[strio.buf.find("<svg"):]
-    return render_template("graph.html",
+        strio.seek(0)
+        svgstr = strio.buf[strio.buf.find("<svg"):]
+        return render_template("graph.html",
                            svgstr=svgstr.decode("utf-8"), title="sensor value")
+    else:
+        return redirect(url_for('index'))
 
 @app.route('/api/', methods=['GET'])
 def get_api():
-    print "GET!"
-    lights = LightValue.query.filter().all()
-    if (len(lights) > 0):
-	result = []
-	for x in lights:
-	    light = { 'mac' : x.mac, 'time' : x.time, 'value' : x.light }
-	    result.append(light)
-	return jsonify({'light' : result})
+    # セッションにemailが保存されていなければ表紙ページにリダイレクトする
+    if session.get('email') is not None:
+        print "GET!"
+        lights = LightValue.query.filter().all()
+        if (len(lights) > 0):
+	    result = []
+	    for x in lights:
+	        light = { 'mac' : x.mac, 'time' : x.time, 'value' : x.light }
+	        result.append(light)
+	    return jsonify({'light' : result})
+        else:
+	    return jsonify(res='error')
     else:
-	return jsonify(res='error') 
+        return redirect(url_for('index'))
 
 @app.route('/api/<int:id>', methods=['GET'])
 def get_api_id(id):
-    lights = LightValue.query.filter(LightValue.time == id).all()
-    if (len(lights) > 0):
-	light = { 'mac' : lights[0].mac, 'time' : lights[0].time, 'value' : lights[0].light }
-	return jsonify({'light' : light})
+    # セッションにemailが保存されていなければ表紙ページにリダイレクトする
+    if session.get('email') is not None:
+        lights = LightValue.query.filter(LightValue.time == id).all()
+        if (len(lights) > 0):
+	    light = { 'mac' : lights[0].mac, 'time' : lights[0].time, 'value' : lights[0].light }
+	    return jsonify({'light' : light})
+        else:
+	    return jsonify(res='error')
     else:
-	return jsonify(res='error') 
+        return redirect(url_for('index'))
 
 @app.route('/api/', methods=['POST'])
 def post_api():
-    print "POST!"
-    if request.headers['Content-Type'] != 'application/json':
-	return jsonify(res='error') 
+    # セッションにemailが保存されていなければ表紙ページにリダイレクトする
+    if session.get('email') is not None:
+        if request.headers['Content-Type'] != 'application/json':
+	    return jsonify(res='error') 
 
-    light = LightValue(request.json['mac'], request.json['time'], request.json['value'])
-    db.session.add(light)
-    db.session.commit()
-    return jsonify(res='ok')
+        light = LightValue(request.json['mac'], request.json['time'], request.json['value'])
+        db.session.add(light)
+        db.session.commit()
+        return jsonify(res='ok')
+    else:
+        return redirect(url_for('index'))
 
 @app.route('/api/', methods=['DELETE'])
 def delete_api():
-    print "DELETE!"
-    lights = LightValue.query.filter().all()
-    if (len(lights) > 0):
-        for x in lights:
-            db.session.delete(x)
-            db.session.commit()
-	return jsonify(res='ok') 
+    # セッションにemailが保存されていなければ表紙ページにリダイレクトする
+    if session.get('email') is not None:
+        lights = LightValue.query.filter().all()
+        if (len(lights) > 0):
+            for x in lights:
+                db.session.delete(x)
+                db.session.commit()
+	    return jsonify(res='ok') 
+        else:
+	    return jsonify(res='error')
     else:
-	return jsonify(res='error') 
+        return redirect(url_for('index'))
 
 @app.route('/api/<int:id>', methods=['DELETE'])
 def delete_api_id(id):
-    print "DELETE!"
-    lights = LightValue.query.filter(LightValue.id == id).first()
-    db.session.delete(lights)
-    db.session.commit()
-    return jsonify(res='ok') 
+    # セッションにemailが保存されていなければ表紙ページにリダイレクトする
+    if session.get('email') is not None:
+        lights = LightValue.query.filter(LightValue.id == id).first()
+        db.session.delete(lights)
+        db.session.commit()
+        return jsonify(res='ok')
+    else:
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
