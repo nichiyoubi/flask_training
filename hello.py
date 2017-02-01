f0308c317a20540896d0bca7177b000fed88fcfd
宇佐美雅紀 <usamimasanori@usamimasakinoMacBook-Pro.local>
2017-02-01 23:58:04 +0900

    layoutを変更

--- hello.py
+++ hello.py
@@ -14,8 +14,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:Masanori1972@localhost/'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:Masanori1972@localhost/'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # 秘密鍵は後ほどランダム化する
 app.config['SECRET_KEY'] = 'The secret key which ciphers the cookie'
